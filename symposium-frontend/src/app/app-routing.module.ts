8b47418e018dbd0dc84bfc77e676aae03d240c66
geokeloglou <geokeloglou@arx.gr>
2021-04-11 21:06:41 +0300

    Basic structure

--- symposium-frontend/src/app/app-routing.module.ts
+++ symposium-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      }, 
+      {
+        path: 'home',
+        component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- symposium-frontend/src/app/app.module.ts
+++ symposium-frontend/src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { HomeComponent } from './views/home/home.component';
+import { MainComponent } from './components/main/main.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavComponent } from './components/nav/nav.component';
+
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent,
+    HomeComponent,
+    MainComponent,
+    ToolbarComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
