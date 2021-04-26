46331306c791f103a8df08ed0cc806f4f5657bd2
geokeloglou <geokeloglou@arx.gr>
2021-04-26 23:54:28 +0300

    Reset password view

---
+++ symposium-frontend/src/app/views/authentication-views/reset-password-view/reset-password-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-password-view',
+  templateUrl: './reset-password-view.component.html',
+  styleUrls: ['./reset-password-view.component.sass']
+})
+export class ResetPasswordViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
