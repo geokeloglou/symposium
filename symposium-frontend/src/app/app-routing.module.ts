dc515538ee3a4da521e34976e432eb6d58b4e362
geokeloglou <geokeloglou@arx.gr>
2021-04-26 23:58:56 +0300

    Routing and imports to general module

--- symposium-frontend/src/app/app-routing.module.ts
+++ symposium-frontend/src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { HomeComponent } from './views/home/home.component';
+import { LoginViewComponent } from './views/authentication-views/login-view/login-view.component';
+import { RegisterViewComponent } from './views/authentication-views/register-view/register-view.component';
+import { ForgotPasswordViewComponent } from './views/authentication-views/forgot-password-view/forgot-password-view.component';
+import { ResetPasswordViewComponent } from './views/authentication-views/reset-password-view/reset-password-view.component';
 
 const routes: Routes = [
   {
@@ -12,17 +16,38 @@ const routes: Routes = [
         path: '',
         redirectTo: 'home',
         pathMatch: 'full'
-      }, 
+      },
       {
         path: 'home',
         component: HomeComponent
       }
     ]
-  }
+  },
+  {
+    path: 'login',
+    component: LoginViewComponent
+  },
+  {
+    path: 'register',
+    component: RegisterViewComponent
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordViewComponent
+  },
+  {
+    path: 'reset-password',
+    component: ResetPasswordViewComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- symposium-frontend/src/app/app.module.ts
+++ symposium-frontend/src/app/app.module.ts
@@ -5,15 +5,42 @@ import { MainComponent } from './components/main/main.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { NavComponent } from './components/nav/nav.component';
 import { PostComponent } from './components/post/post.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NotifierComponent } from './components/notifier/notifier.component';
 
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { TokenInterceptor } from './helpers/token.interceptor';
+
+import { AuthService } from './services/auth.service';
+import { TokenService } from './services/token.service';
+
+import { AuthGuard } from './guards/auth.guard';
+import { JwtModule } from '@auth0/angular-jwt';
+import { LoginViewComponent } from './views/authentication-views/login-view/login-view.component';
+import { RegisterViewComponent } from './views/authentication-views/register-view/register-view.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { ForgotPasswordViewComponent } from './views/authentication-views/forgot-password-view/forgot-password-view.component';
+import { ResetPasswordViewComponent } from './views/authentication-views/reset-password-view/reset-password-view.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+
+export function tokenGetter(): string | null {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
   declarations: [
@@ -23,18 +50,51 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MainComponent,
     ToolbarComponent,
     NavComponent,
-    PostComponent
+    PostComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotifierComponent,
+    LoginViewComponent,
+    RegisterViewComponent,
+    ForgotPasswordComponent,
+    ForgotPasswordViewComponent,
+    ResetPasswordViewComponent,
+    ResetPasswordComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSnackBarModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter,
+        allowedDomains: ['http://localhost:4200/'],
+        disallowedRoutes: [''],
+      },
+    }),
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuard,
+    TokenService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
