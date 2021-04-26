f0e3a3e706c049aa0e7c9455bf9e333189fdf58e
geokeloglou <geokeloglou@arx.gr>
2021-04-26 23:58:13 +0300

    Notifier component

---
+++ symposium-frontend/src/app/components/notifier/notifier.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-notifier',
+  templateUrl: './notifier.component.html',
+  styleUrls: ['./notifier.component.sass']
+})
+export class NotifierComponent implements OnInit {
+
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any,
+              public snackBarRef: MatSnackBarRef<NotifierComponent>) {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
