924a4f89174027b19291a229ea823df291eb2817
geokeloglou <geokeloglou@arx.gr>
2021-04-26 23:55:24 +0300

    Notifier service

---
+++ symposium-frontend/src/app/services/notifier.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NotifierComponent } from '../components/notifier/notifier.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotifierService {
+
+  constructor(private snackBar: MatSnackBar) {
+  }
+
+  showNotification(
+    displayMessage: string,
+    buttonText: string,
+    messageType: 'error' | 'success'
+  ): void {
+    this.snackBar.openFromComponent(NotifierComponent, {
+      data: {
+        message: displayMessage,
+        buttonText,
+        type: messageType,
+      },
+      duration: 2500,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+      panelClass: messageType,
+    });
+  }
+}
