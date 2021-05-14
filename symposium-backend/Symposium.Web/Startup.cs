f12bbe8c82b8bc8425111413860aad31513ff1a9
geokeloglou <geokeloglou@arx.gr>
2021-05-15 01:08:44 +0300

    Add services on startup

--- symposium-backend/Symposium.Web/Startup.cs
+++ symposium-backend/Symposium.Web/Startup.cs
@@ -1,9 +1,17 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Symposium.Data.Database;
+using Symposium.Services.AuthenticationService;
+using Symposium.Services.EmailService;
+using Symposium.Services.Utilities;
 
 namespace Symposium.Web
 {
@@ -28,6 +36,33 @@ namespace Symposium.Web
                 .AllowAnyMethod()
                 .AllowAnyHeader())); 
             services.AddControllers();
+            
+            services.AddDbContext<SymposiumDbContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetSection("ConnectionStrings:DefaultConnection").Value);
+                options.EnableSensitiveDataLogging();
+            });
+            services.AddPgConnection(Configuration.GetSection("ConnectionStrings:DefaultConnection").Value, "public");
+            services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader())); 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                            .GetBytes(Configuration.GetSection("AuthSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+            services.AddControllers();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton(Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>());
+            services.AddScoped<IEmailSender, EmailSender>();;
+            services.AddScoped<IUserAuthenticationService, UserAuthenticationService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
