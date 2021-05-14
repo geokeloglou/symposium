3eff310b08ba09df4f53d153e85301256f356f23
geokeloglou <geokeloglou@arx.gr>
2021-05-15 01:12:14 +0300

    Add RegisterUserDto

---
+++ symposium-backend/Symposium.DTO/AuthenticationDto/RegisterUserDto.cs
@@ -0,0 +1,11 @@
+namespace Symposium.DTO.AuthenticationDto
+{
+    public class RegisterUserDto
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Intro { get; set; }
+    }
+}
