7d392657b2e7c2d39d17abc10f76b2edf7db7444
geokeloglou <geokeloglou@arx.gr>
2021-05-15 15:07:19 +0300

    Fix register user dto

--- symposium-backend/Symposium.DTO/AuthenticationDto/RegisterUserDto.cs
+++ symposium-backend/Symposium.DTO/AuthenticationDto/RegisterUserDto.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Symposium.DTO.AuthenticationDto
 {
     public class RegisterUserDto
     {
+        [Required]
         public string Firstname { get; set; }
+        
+        [Required]
         public string Lastname { get; set; }
+        
+        [Required, EmailAddress]
         public string Email { get; set; }
+        
+        [Required]
         public string Password { get; set; }
-        public string Intro { get; set; }
+        
+        public string? Intro { get; set; }
     }
 }
