9454b8aacda47fe53424804015e3c6b618831a11
geokeloglou <geokeloglou@gmail.com>
2021-05-16 19:56:49 +0300

    Logout functionality

--- symposium-frontend/src/app/components/nav/nav.component.ts
+++ symposium-frontend/src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { NotifierService } from '../../services/notifier.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private notifierService: NotifierService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  logout(): void {
+    this.authService.logout().then(() => {
+      this.notifierService.showNotification(
+        'Logged out successfully.',
+        'OK',
+        'success',
+      );
+    }).catch(() => {
+      this.notifierService.showNotification(
+        'Log out failed.',
+        'OK',
+        'error',
+      );
+    });
   }
 
 }
