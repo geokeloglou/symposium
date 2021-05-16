962b5e76dc9dcac1f108e2852c7b7a137e8cf744
geokeloglou <geokeloglou@gmail.com>
2021-05-16 19:55:23 +0300

    Fix auth service

--- symposium-frontend/src/app/services/auth.service.ts
+++ symposium-frontend/src/app/services/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService extends BaseService {
 
   login(user: LoginRequest): Observable<ApiResponse> {
     return this.httpClient
-      .post<ApiResponse>(`${ this.baseUrl }/authentication/login`, user)
+      .post<ApiResponse>(`${ this.baseUrl }/api/auth/login`, user)
       .pipe(
         tap((response: ApiResponse) => this.doLoginUser(response)),
         catchError((error) => this.handleError(error))
@@ -35,7 +35,7 @@ export class AuthService extends BaseService {
 
   register(user: any): Observable<ApiResponse> {
     return this.httpClient
-      .post<ApiResponse>(`${ this.baseUrl }/authentication/register`, user)
+      .post<ApiResponse>(`${ this.baseUrl }/api/auth/register`, user)
       .pipe(
         tap(() => this.router.navigate(['login'])),
         catchError((error) => this.handleError(error))
@@ -45,7 +45,7 @@ export class AuthService extends BaseService {
   forgotPassword(email: string): Observable<boolean | ApiResponse> {
     return this.httpClient
       .post<ApiResponse>(
-        `${ this.baseUrl }/authentication/forgot-password`,
+        `${ this.baseUrl }/api/auth/forgot`,
         email
       )
       .pipe(
@@ -56,14 +56,10 @@ export class AuthService extends BaseService {
 
   resetPassword(body: { token: string; password: string }): Observable<ApiResponse> {
     return this.httpClient
-      .post<ApiResponse>(
-        `${ this.baseUrl }/authentication/reset-password`,
-        body
-      )
+      .post<ApiResponse>(`${ this.baseUrl }/api/auth/reset`, body)
       .pipe(
         tap(() => this.router.navigate(['login'])),
-        catchError((error) => this.handleError(error))
-      );
+        catchError((error) => this.handleError(error)));
   }
 
   isLoggedIn(): boolean {
@@ -82,8 +78,15 @@ export class AuthService extends BaseService {
     this.removeJwtToken();
   }
 
+  logout(): Promise<boolean> {
+    this.removeJwtToken();
+    return this.router.navigate(['/login']);
+  }
+
   private doLoginUser(response: ApiResponse): void {
-    this.storeToken(response.data!.toString());
+    if (response.data !== null && response.data !== undefined) {
+      this.storeToken(response.data.toString());
+    }
   }
 
   private storeToken(token: string): void {
