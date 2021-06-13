e673a95ffb60b1618f77400ec846d988fa0b6683
geokeloglou <geokeloglou@gmail.com>
2021-06-13 22:55:39 +0300

    Add Post controller - create / get / get all

---
+++ symposium-backend/Symposium.Web/Controllers/Api/PostControllers/PostController.cs
@@ -0,0 +1,56 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Symposium.DTO.PostDto;
+using Symposium.Services.PostService;
+
+namespace Symposium.Web.Controllers.Api.PostControllers
+{
+    [ApiController]
+    [Route("api/post")]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostService _postService;
+
+        public PostController(
+            IPostService postService)
+        {
+            _postService = postService;
+        }
+        
+        [HttpPost("create")]
+        public async Task<IActionResult> CreatePost(CreatePostDto post)
+        {
+            var response = await _postService.CreatePost(post);
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+        
+        [HttpGet("get")]
+        public async Task<IActionResult> GetPosts()
+        {
+            var response = await _postService.GetPosts();
+            if (!response.Success)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
+        
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAllPosts()
+        {
+            var response = await _postService.GetAllPosts();
+            if (!response.Success)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
+    }
+}
