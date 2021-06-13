077ae35508da0832dd5345952fe3756a39f9cdac
geokeloglou <geokeloglou@gmail.com>
2021-06-14 01:02:14 +0300

    Provide PostSandbox

--- symposium-frontend/src/app/components/feed/feed.component.ts
+++ symposium-frontend/src/app/components/feed/feed.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { PostSandbox } from './post.sandbox';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.sass']
+  styleUrls: ['./feed.component.sass'],
+  providers: [PostSandbox]
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  constructor(public postSandbox: PostSandbox) {
+  }
 
   ngOnInit(): void {
   }
