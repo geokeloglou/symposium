233b977d84aca97c654eac7bf0f344e45eaf85d1
geokeloglou <geokeloglou@gmail.com>
2021-06-28 01:53:34 +0300

    Add createPost functionality to sandbox

--- symposium-frontend/src/app/components/feed/post/post.sandbox.ts
+++ symposium-frontend/src/app/components/feed/post/post.sandbox.ts
@@ -1,5 +1,5 @@
 import { BehaviorSubject, Observable, Subscription } from 'rxjs';
-import { PostData } from '../../../models/post.interface';
+import { CreatePost, PostData } from '../../../models/post.interface';
 import { PostService } from '../../../services/post.service';
 import { catchError, distinctUntilChanged, filter, map } from 'rxjs/operators';
 import { NotifierService } from '../../../services/notifier.service';
@@ -11,6 +11,7 @@ export class PostSandbox implements OnDestroy {
 
   private getAllPostsSubscription: Subscription;
   private _posts$: BehaviorSubject<PostData[]> = new BehaviorSubject<PostData[]>([]);
+  private createPostSubscription: Subscription;
 
   constructor(private postService: PostService, private notifierService: NotifierService) {
     this.init();
@@ -27,7 +28,7 @@ export class PostSandbox implements OnDestroy {
   private getAllPosts(): void {
     this.getAllPostsSubscription = this.postService.getAllPosts()
       .pipe(
-        catchError(error => {
+        catchError((error: ApiResponse) => {
           this.notifierService.showNotification(error.message, 'OK', 'error');
           return [];
         }),
@@ -38,8 +39,20 @@ export class PostSandbox implements OnDestroy {
       .subscribe(posts => this._posts$.next(posts as PostData[]));
   }
 
+  createPost(post: CreatePost): void {
+    this.createPostSubscription?.unsubscribe();
+    this.createPostSubscription = this.postService.createPost(post)
+      .subscribe((response: ApiResponse) => {
+        this.getAllPosts();
+        this.notifierService.showNotification(response.message, 'OK', 'success');
+      }, (error: ApiResponse) => {
+        this.notifierService.showNotification(error.message, 'OK', 'error');
+      });
+  }
+
   ngOnDestroy(): void {
     this.getAllPostsSubscription.unsubscribe();
+    this.createPostSubscription.unsubscribe();
     this._posts$.complete();
     this._posts$.next([]);
   }
