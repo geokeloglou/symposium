11426bff78cb8dec2ecaf8330aa4a4c4825b4848
geokeloglou <geokeloglou@gmail.com>
2021-06-28 01:52:59 +0300

    Add 'add post dialog' component

---
+++ symposium-frontend/src/app/components/feed/add-post-dialog/add-post-dialog.component.ts
@@ -0,0 +1,37 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-post-dialog',
+  templateUrl: './add-post-dialog.component.html',
+  styleUrls: ['./add-post-dialog.component.sass']
+})
+export class AddPostDialogComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private dialogRef: MatDialogRef<AddPostDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      text: ['', Validators.required]
+    });
+  }
+
+  save(): void {
+    if (!this.errorExists()) {
+      this.dialogRef.close(this.form.value);
+    }
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
+
+  errorExists(): boolean {
+    return this.form.get('text')?.value.replace(/\s/g, '') === '';
+  }
+
+}
