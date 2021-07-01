c4323e65a37fefbecaef62813d60c5b5836eb2ee
geokeloglou <geokeloglou@gmail.com>
2021-07-01 21:29:38 +0300

    Add Input for postImageUrl

--- symposium-frontend/src/app/components/feed/post/post.component.ts
+++ symposium-frontend/src/app/components/feed/post/post.component.ts
@@ -10,6 +10,7 @@ export class PostComponent {
   @Input() firstname: string;
   @Input() lastname: string;
   @Input() username: string;
+  @Input() postImageUrl: string;
   @Input() date: Date;
   @Input() text: string;
   @Input() likes: number;
