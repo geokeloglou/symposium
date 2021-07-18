d2b0f905660d626ae15a0aaa92de3691ac156170
geokeloglou <geokeloglou@gmail.com>
2021-07-18 21:16:13 +0300

    Update feed sandbox to support uploadProfileImage

--- symposium-frontend/src/app/components/feed/feed.sandbox.ts
+++ symposium-frontend/src/app/components/feed/feed.sandbox.ts
@@ -5,15 +5,17 @@ import { BehaviorSubject, Subscription } from 'rxjs';
 import { Profile } from '../../models/profile.interface';
 import { catchError, filter, map } from 'rxjs/operators';
 import { ApiResponse } from '../../models/http.interface';
+import { PostSandbox } from './post/post.sandbox';
 
 @Injectable()
 export class FeedSandbox implements OnDestroy {
 
   private getUserProfileInfoSubscription: Subscription;
+  private uploadProfileImageSubscription: Subscription;
   private _profile$ = new BehaviorSubject<Profile[]>([]);
   imageUrl: string;
 
-  constructor(private profileService: ProfileService, private notifierService: NotifierService) {
+  constructor(private profileService: ProfileService, private notifierService: NotifierService, private postSandbox: PostSandbox) {
     this.init();
   }
 
@@ -35,6 +37,17 @@ export class FeedSandbox implements OnDestroy {
       .subscribe(profile => {
         this._profile$.next(profile as Profile[]);
         this.imageUrl = (profile as Profile[]).map(p => p.imageUrl).toString();
+      });
+  }
+
+  uploadProfileImage(profileImage: string | Blob): void {
+    this.uploadProfileImageSubscription = this.profileService.uploadProfileImage(profileImage)
+      .subscribe((response: ApiResponse) => {
+        this.getUserProfileInfo();
+        this.postSandbox.getAllPosts();
+        this.notifierService.showNotification(response.message, 'OK', 'success');
+      }, (error: ApiResponse) => {
+        this.notifierService.showNotification(error.message, 'OK', 'error');
       });
   }
 
